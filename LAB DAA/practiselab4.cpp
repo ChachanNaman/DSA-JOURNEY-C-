66c2ae69920fbbc022685e8be0afba9016f01a3c
Naman <naman.chachan@mitwpu.edu.in>
2025-04-20 02:39:07 +0530

    avl

--- LAB DAA/practiselab4.cpp
+++ LAB DAA/practiselab4.cpp
@@ -41,10 +41,10 @@ public:
             return curr;
         }
         else if(curr->word < root->word){
-            root->left = avl_rec(root , curr->left);
+            root->left = avl_rec(root->left , curr);
         }
         else{
-            root->right = avl_rec(root , curr->right);
+            root->right = avl_rec(root->right , curr);
         }
         return balance(root);
     }
@@ -67,7 +67,7 @@ public:
                 return RR_rotation(temp);
             }
             else{
-                cout<<"\nLL rotation at "<<temp->word<<endl;
+                cout<<"\nRL rotation at "<<temp->word<<endl;
                 return RL_rotation(temp);
             }
         }
@@ -85,6 +85,49 @@ public:
             return max(height(temp->left) , height(temp->right)) +1;
         }
     }
+    node * LL_rotation(node *temp){
+        node *x = temp->left;
+        node *y = x->right;
+        x->right = temp;
+        temp->left = y;
+        return x;
+    }
+    node *RR_rotation(node *temp){
+        node *x = temp->right;
+        node * y = x->left;
+        x->left = temp;
+        temp->right = y;
+        return x;
+    }
+    node *LR_rotation(node *temp){
+        temp->left = RR_rotation(temp->left);
+        return LL_rotation(temp);
+    }
+    node*RL_rotation(node *temp){
+        temp->right = LL_rotation(temp->right);
+        return RR_rotation(temp);
+    }
+    void inorder_rec(node *root)
+    {
+        if (root != NULL)
+        {
+            inorder_rec(root->left);
+            cout << root->word << " : " << root->mean << endl;
+            inorder_rec(root->right);
+        }
+    }
 
+    void display()
+    {
+        cout << "Inorder view of my data: " << endl;
+        inorder_rec(root);
+    }
 };
+
+int main(){
+    avl a;
+    a.create();
+    a.display();
+    return 0;
+}
 
