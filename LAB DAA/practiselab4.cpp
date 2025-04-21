88d0746c6d3be8660aea301b12eaf09823725688
Naman <naman.chachan@mitwpu.edu.in>
2025-04-22 01:15:50 +0530

    pracc

--- LAB DAA/practiselab4.cpp
+++ LAB DAA/practiselab4.cpp
@@ -20,90 +20,111 @@ public:
     {
         root = NULL;
     }
-    void create(){
+    void create()
+    {
         char ch;
         node *curr;
         string word, mean;
-        do{
-        cout<<"enter the word and mean : ";
-        cin>>word>>mean;
-        curr = new node();
-        curr->word = word;
-        curr->mean = mean;
-        curr->left = curr -> right = NULL;
-        root = avl_rec(root , curr);
-        cout<<"Do you want to add another word ? (y/n) : ";
-        cin>>ch;
-        }while(ch =='y');
+        do
+        {
+            cout << "enter the word and mean : ";
+            cin >> word >> mean;
+            curr = new node();
+            curr->word = word;
+            curr->mean = mean;
+            curr->left = curr->right = NULL;
+            root = avl_rec(root, curr);
+            cout << "Do you want to add another word ? (y/n) : ";
+            cin >> ch;
+        } while (ch == 'y');
     }
-    node *avl_rec(node *root , node *curr){
-        if(root == NULL){
+    node *avl_rec(node *root, node *curr)
+    {
+        if (root == NULL)
+        {
             return curr;
         }
-        else if(curr->word < root->word){
-            root->left = avl_rec(root->left , curr);
+        else if (curr->word < root->word)
+        {
+            root->left = avl_rec(root->left, curr);
         }
-        else{
-            root->right = avl_rec(root->right , curr);
+        else
+        {
+            root->right = avl_rec(root->right, curr);
         }
         return balance(root);
     }
-    node *balance(node *temp){
+    node *balance(node *temp)
+    {
         int bf;
         bf = BF(temp);
-        if(bf == 2){
-            if(BF(temp->left)<0){
-                cout<<"\nLR rotation at "<<temp->word<<endl;
+        if (bf == 2)
+        {
+            if (BF(temp->left) < 0)
+            {
+                cout << "\nLR rotation at " << temp->word << endl;
                 return LR_rotation(temp);
             }
-            else{
-                cout<<"\nLL rotation at "<<temp->word<<endl;
+            else
+            {
+                cout << "\nLL rotation at " << temp->word << endl;
                 return LL_rotation(temp);
             }
         }
-        if(bf == -2){
-            if(BF(temp->left)<0){
-                cout<<"\nRR rotation at "<<temp->word<<endl;
-                return RR_rotation(temp);
-            }
-            else{
-                cout<<"\nRL rotation at "<<temp->word<<endl;
+        if (bf == -2)
+        {
+            if (BF(temp->right) > 0)
+            {
+                cout << "\nRL rotation at " << temp->word << endl;
                 return RL_rotation(temp);
+            }
+            else
+            {
+                cout << "\nRR rotation at " << temp->word << endl;
+                return RR_rotation(temp);
             }
         }
         return temp;
     }
-    int BF(node *temp){
-        int bf = height(temp->left)-height(temp->right);
+    int BF(node *temp)
+    {
+        int bf = height(temp->left) - height(temp->right);
         return bf;
     }
-    int height(node*temp){
-        if(temp == NULL){
+    int height(node *temp)
+    {
+        if (temp == NULL)
+        {
             return 0;
         }
-        else{
-            return max(height(temp->left) , height(temp->right)) +1;
+        else
+        {
+            return max(height(temp->left), height(temp->right)) + 1;
         }
     }
-    node * LL_rotation(node *temp){
+    node *LL_rotation(node *temp)
+    {
         node *x = temp->left;
         node *y = x->right;
         x->right = temp;
         temp->left = y;
         return x;
     }
-    node *RR_rotation(node *temp){
+    node *RR_rotation(node *temp)
+    {
         node *x = temp->right;
-        node * y = x->left;
+        node *y = x->left;
         x->left = temp;
         temp->right = y;
         return x;
     }
-    node *LR_rotation(node *temp){
+    node *LR_rotation(node *temp)
+    {
         temp->left = RR_rotation(temp->left);
         return LL_rotation(temp);
     }
-    node*RL_rotation(node *temp){
+    node *RL_rotation(node *temp)
+    {
         temp->right = LL_rotation(temp->right);
         return RR_rotation(temp);
     }
@@ -122,12 +143,43 @@ public:
         cout << "Inorder view of my data: " << endl;
         inorder_rec(root);
     }
+    void search(string key)
+    {
+        int count = 0;
+        node *temp = root;
+        while (temp != NULL)
+        {
+            count++;
+            if (temp->word == key)
+            {
+                cout << "word found ! " << endl;
+                cout << "no. of comparisons : " << count << endl;
+                return;
+            }
+            else if (key < temp->word)
+            {
+                temp = temp->left;
+            }
+            else
+            {
+                temp = temp->right;
+            }
+        }
+        cout << "no word is found sorry" << endl;
+        cout << "no. of comparisson it took : " << count << endl;
+    }
 };
 
-int main(){
+int main()
+{
     avl a;
     a.create();
     a.display();
+
+    string key;
+    cout << "enter word to search : ";
+    cin >> key;
+    a.search(key);
+
     return 0;
 }
-
--- LAB DAA/tempCodeRunnerFile.cpp
+++ LAB DAA/tempCodeRunnerFile.cpp
@@ -1,112 +1,185 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class graph{
-    int cities;
-    int cost[10][10];
-    public:
-        graph(){
-            cout << "Enter no. of cities: ";
-            cin >> cities;
-            for(int i = 0; i < cities; i++){
-                for(int j = 0; j < cities; j++){
-                    cost[i][j] = 999; 
-                }
-            }
-        }
-
-        void create(){
-            char ch;
-            for(int i = 0; i < cities; i++){
-                for(int j = 0; j < cities; j++){
-                    if(i != j and cost[i][j] == 999){
-                        cout << "Is there connection between " << i << " and " << j << "? (y/n): ";
-                        cin >> ch;
-                        if (ch == 'y'){
-                            cout << "Enter cost: ";
-                            cin >> cost[i][j];
-                            cost[j][i] = cost[i][j];
-                        } 
-                    }
-                }
-            }
-        }
-
-        void display(){
-            cout <<  "The adjacency matrix is: \n";
-            for(int i = 0; i < cities; i++){
-                for(int j = 0; j < cities; j++){
-                    cout << cost[i][j] << "\t";
-                }
-                cout << endl;
-            }
-        }
-
-        int primsAlgorithm(int start){
-            int nearest[cities];
-            int n = cities;
-            int t[cities][3];
-            int r = 0;
-            int minCost = 0;
-            int min;
-            int index;
-            nearest[start] = -1;
-
-            for(int i = 0; i < n; i++){
-                if(i != start){
-                    nearest[i] = start;
-                    }   
-               
-            }
-
-            for(int i = 0; i < n - 1; i++){ 
-                min = 999;
-                for(int j = 0; j < n - 1; j++){
-                    if(nearest[j] != -1 and cost[j][nearest[j]] < min){  
-                        index = j;   
-                        min = cost[j][nearest[j]];     
-                    }
-                }
-
-                t[r][0] = nearest[index];    
-                t[r][1] = index;   
-                t[r][2] = min;    
-                r++;
-                minCost += cost[index][nearest[index]];   
-                nearest[index] = -1;
-
-                for(int j = 0; j < n ; j++){
-                    if(nearest[j] != -1 and (cost[j][nearest[j]] > cost[j][index]))
-                     {                       
-                        nearest[j] = index;  
-                     }
-                }
-            }
-            return minCost;
-        }
+class node
+{
+    string word;
+    string mean;
+    node *left;
+    node *right;
+    friend class avl;
 };
 
-int main(){
-    graph g;
-    int ch;
-    while(1){
-        cout << "1. Accept matrix\n2. Display matrix\n3. Find MST\n4. Exit\n";
-        cout << "Enter choice: ";
-        cin >> ch;
-        switch(ch){
-            case 1:
-                g.create();
-                break;
-            case 2:
-                g.display();
-                break;
-            case 3:
+class avl
+{
+    node *root;
 
-                cout << "The cost is: " << g.primsAlgorithm(0) << endl;
-                break;
-            case 4:
-                cout << "Thank You!";
-                return 0;
+public:
+    avl()
+    {
+        root = NULL;
+    }
+    void create()
+    {
+        char ch;
+        node *curr;
+        string word, mean;
+        do
+        {
+            cout << "enter the word and mean : ";
+            cin >> word >> mean;
+            curr = new node();
+            curr->word = word;
+            curr->mean = mean;
+            curr->left = curr->right = NULL;
+            root = avl_rec(root, curr);
+            cout << "Do you want to add another word ? (y/n) : ";
+            cin >> ch;
+        } while (ch == 'y');
+    }
+    node *avl_rec(node *root, node *curr)
+    {
+        if (root == NULL)
+        {
+            return curr;
+        }
+        else if (curr->word < root->word)
+        {
+            root->left = avl_rec(root->left, curr);
+        }
+        else
+        {
+            root->right = avl_rec(root->right, curr);
+        }
+        return balance(root);
+    }
+    node *balance(node *temp)
+    {
+        int bf;
+        bf = BF(temp);
+        if (bf == 2)
+        {
+            if (BF(temp->left) < 0)
+            {
+                cout << "\nLR rotation at " << temp->word << endl;
+                return LR_rotation(temp);
+            }
+            else
+            {
+                cout << "\nLL rotation at " << temp->word << endl;
+                return LL_rotation(temp);
+            }
+        }
+        if (bf == -2)
+        {
+            if (BF(temp->left) < 0)
+            {
+                cout << "\nRR rotation at " << temp->word << endl;
+                return RR_rotation(temp);
+            }
+            else
+            {
+                cout << "\nRL rotation at " << temp->word << endl;
+                return RL_rotation(temp);
+            }
+        }
+        return temp;
+    }
+    int BF(node *temp)
+    {
+        int bf = height(temp->left) - height(temp->right);
+        return bf;
+    }
+    int height(node *temp)
+    {
+        if (temp == NULL)
+        {
+            return 0;
+        }
+        else
+        {
+            return max(height(temp->left), height(temp->right)) + 1;
         }
     }
+    node *LL_rotation(node *temp)
+    {
+        node *x = temp->left;
+        node *y = x->right;
+        x->right = temp;
+        temp->left = y;
+        return x;
+    }
+    node *RR_rotation(node *temp)
+    {
+        node *x = temp->right;
+        node *y = x->left;
+        x->left = temp;
+        temp->right = y;
+        return x;
+    }
+    node *LR_rotation(node *temp)
+    {
+        temp->left = RR_rotation(temp->left);
+        return LL_rotation(temp);
+    }
+    node *RL_rotation(node *temp)
+    {
+        temp->right = LL_rotation(temp->right);
+        return RR_rotation(temp);
+    }
+    void inorder_rec(node *root)
+    {
+        if (root != NULL)
+        {
+            inorder_rec(root->left);
+            cout << root->word << " : " << root->mean << endl;
+            inorder_rec(root->right);
+        }
+    }
+
+    void display()
+    {
+        cout << "Inorder view of my data: " << endl;
+        inorder_rec(root);
+    }
+    void search(string key)
+    {
+        int count;
+        node *temp = root;
+        while (temp != NULL)
+        {
+            count++;
+            if (temp->word == key)
+            {
+                cout << "word found ! " << endl;
+                cout << "no. of comparisons : " << count << endl;
+                return;
+            }
+            else if (key < temp->word)
+            {
+                temp = temp->left;
+            }
+            else
+            {
+                temp = temp->right;
+            }
+        }
+        cout << "no word is found sorry" << endl;
+        cout << "no. of comparisson it took : " << count << endl;
+    }
+};
+
+int main()
+{
+    avl a;
+    a.create();
+    a.display();
+
+    string key;
+    cout << "enter word to search : ";
+    cin >> key;
+    a.search(key);
+
+    return 0;
 }
